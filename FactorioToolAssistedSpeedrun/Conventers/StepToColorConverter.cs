247d165b8ccab876071312b8d823c0b208a2ea39
VINAGHOST <nguyentrungnhan87@gmail.com>
2026-01-09 19:29:15 +0700

    Add StepToColorConverter for colored step highlighting
    Introduced StepToColorConverter to map StepType to cell background colors in the Step column of the DataGrid. Updated StepPanel.xaml to use this converter and apply a custom cell style, improving visual distinction of step types and selection contrast.

---
+++ FactorioToolAssistedSpeedrun/Conventers/StepToColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using FactorioToolAssistedSpeedrun.Enums;
+using FactorioToolAssistedSpeedrun.Models.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FactorioToolAssistedSpeedrun.Conventers
+{
+    public class StepToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not StepModel step) throw new ArgumentException("Value must be of type StepModel", nameof(value));
+
+            //if (!string.IsNullOrEmpty(step.Color)) return Color.FromName(step.Color);
+
+            return step.Type switch
+            {
+                StepType.Build => "Cyan",
+                StepType.Stop => "Red",
+                StepType.Craft => "LightGray",
+                StepType.Speed or StepType.Pause or StepType.Save or StepType.KeepCrafting or StepType.KeepOnPath or StepType.KeepWalking or StepType.NeverIdle => "Yellow",
+                _ => "White",
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

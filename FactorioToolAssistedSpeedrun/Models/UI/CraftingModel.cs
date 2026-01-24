b2ca4f08cda9a03dbe59cc2c5a51cf17ab20e085
VINAGHOST <nguyentrungnhan87@gmail.com>
2026-01-24 15:09:14 +0700

    Enable two-way editing and undo/redo in crafting UI
    Added two-way data binding for step properties in the crafting DataGrid, with validation and command-based undo/redo support. Introduced item name auto-complete using ItemsCollection. Updated model and viewmodel to support direct, validated editing and improved data integrity.

--- FactorioToolAssistedSpeedrun/Models/UI/CraftingModel.cs
+++ FactorioToolAssistedSpeedrun/Models/UI/CraftingModel.cs
@@ -1,12 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FactorioToolAssistedSpeedrun.Commands.Steps;
 using FactorioToolAssistedSpeedrun.Entities;
 using FactorioToolAssistedSpeedrun.Enums;
+using FactorioToolAssistedSpeedrun.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System.Xml.Linq;
 
 namespace FactorioToolAssistedSpeedrun.Models.UI
 {
     public partial class CraftingModel : ObservableObject
     {
+        private readonly CommandStack _commandStack = App.Current.Services.GetRequiredService<CommandStack>();
+        private readonly StartupService _startupService = App.Current.Services.GetRequiredService<StartupService>();
         public Guid Id { get; set; }
+
+        private bool _loaded = false;
+        private bool _lock = false;
 
         [ObservableProperty]
         private StepType _type;
@@ -17,18 +26,121 @@ namespace FactorioToolAssistedSpeedrun.Models.UI
         [ObservableProperty]
         private string _item = "";
 
+        partial void OnItemChanged(string? oldValue, string newValue)
+        {
+            if (!_loaded) return;
+            if (_lock) return;
+
+            _lock = true;
+
+            if (!_startupService.GameData!.Items.ContainsKey(newValue))
+            {
+                Item = oldValue ?? "";
+            }
+            else
+            {
+                var command = _commandStack.Push<UpdateStepPropertyCommand<string, string>>();
+                if (command is not null)
+                {
+                    command.Setup(new("", Id, oldValue ?? "", newValue,
+                        str => str,
+                        model => model.Item,
+                        step => step.Item));
+
+                    command.Commit();
+                }
+            }
+
+            _lock = false;
+        }
+
         [ObservableProperty]
         private string _amount = "";
+
+        partial void OnAmountChanged(string? oldValue, string newValue)
+        {
+            if (!_loaded) return;
+            if (_lock) return;
+            if (newValue == "All") newValue = "0";
+
+            _lock = true;
+
+            if (!double.TryParse(newValue, out var value))
+            {
+                Amount = oldValue ?? "";
+            }
+            else
+            {
+                if (value == 0)
+                {
+                    Amount = "All";
+                }
+                else if (value < 0)
+                {
+                    Amount = oldValue ?? "";
+                }
+                else
+                {
+                    var command = _commandStack.Push<UpdateStepPropertyCommand<string, int>>();
+                    if (command is not null)
+                    {
+                        command.Setup(new("", Id, oldValue ?? "", newValue,
+                            str => int.Parse(str),
+                            model => model.Amount,
+                            step => step.Amount));
+
+                        command.Commit();
+                    }
+                }
+            }
+
+            _lock = false;
+        }
 
         [ObservableProperty]
         private string _comment = "";
 
+        partial void OnCommentChanged(string? oldValue, string newValue)
+        {
+            if (!_loaded) return;
+            if (_lock) return;
+
+            var command = _commandStack.Push<UpdateStepPropertyCommand<string, string>>();
+            if (command is not null)
+            {
+                command.Setup(new("", Id, oldValue ?? "", newValue,
+                    str => str,
+                    model => model.Comment,
+                    step => step.Comment));
+
+                command.Commit();
+            }
+        }
+
         [ObservableProperty]
         private bool _isSkip;
+
+        partial void OnIsSkipChanged(bool oldValue, bool newValue)
+        {
+            if (!_loaded) return;
+            if (_lock) return;
+
+            var command = _commandStack.Push<UpdateStepPropertyCommand<bool, bool>>();
+            if (command is not null)
+            {
+                command.Setup(new("", Id, oldValue, newValue,
+                    str => str,
+                    model => model.IsSkip,
+                    step => step.IsSkip));
+
+                command.Commit();
+            }
+        }
 
         public void FromEntity(Step step)
         {
             if (step.Type != StepType.Craft) return;
+            _loaded = false;
 
             Id = step.Id;
             Type = step.Type;
@@ -45,6 +157,7 @@ namespace FactorioToolAssistedSpeedrun.Models.UI
             {
                 Amount = $"{step.Amount}";
             }
+            _loaded = true;
         }
     }
 }
--- FactorioToolAssistedSpeedrun/ViewModels/CraftingViewModel.cs
+++ FactorioToolAssistedSpeedrun/ViewModels/CraftingViewModel.cs
@@ -8,17 +8,21 @@ namespace FactorioToolAssistedSpeedrun.ViewModels
     public partial class CraftingViewModel : ObservableObject
     {
         private readonly PanelService _panelService;
+        private readonly StartupService _startupService;
         public PanelService PanelService => _panelService;
+        public StartupService StartupService => _startupService;
 
         public CraftingViewModel()
         {
             _panelService = App.Current.Services.GetRequiredService<PanelService>();
+            _startupService = App.Current.Services.GetRequiredService<StartupService>();
         }
 
         [ActivatorUtilitiesConstructor]
-        public CraftingViewModel(PanelService panelService)
+        public CraftingViewModel(PanelService panelService, StartupService startupService)
         {
             _panelService = panelService;
+            _startupService = startupService;
         }
 
         [RelayCommand]
